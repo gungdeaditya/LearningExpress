853fee439098eddf32dd0b14335a4305564d239d
M Haidar Hanif <git@mhaidarhanif.com>
2017-05-16 17:36:48 +0700

    Create User model and migration

---
+++ models/user.js
@@ -0,0 +1,16 @@
+'use strict'
+
+module.exports = function (sequelize, DataTypes) {
+  const User = sequelize.define('User', {
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function (models) {
+        // associations can be defined here
+      }
+    }
+  })
+  return User
+}
